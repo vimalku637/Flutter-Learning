d3dbbd1d33a7f42adacc05cb87ca9ca0a8540df3
Vimal Kumar <vimalku637@gmail.com>
2025-10-15 21:25:45 +0530

    Add logic to your app part-4

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatelessWidget{
@@ -9,12 +11,18 @@ class FirstScreen extends StatelessWidget{
       color: Colors.amberAccent,
       child: Center(
         child: Text(
-          "Hello Flutter.!!",
+          generateLuckyNumber(),
           textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.black, fontSize: 40.0),
+          style: TextStyle(color: Colors.black, fontSize: 30.0),
         ), //Text
       ), //Center
     ); //Material
-    throw UnimplementedError();
+  }
+
+  //write some logical code
+  String generateLuckyNumber(){
+    var random = Random();
+    var luckyNumber = random.nextInt(10);
+    return "Your lucky number is $luckyNumber";
   }
 }
