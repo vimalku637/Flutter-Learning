5680c422fed190c9569e4af90f2fbb89b949cc77
Vimal Kumar <vimalku637@gmail.com>
2025-10-15 20:56:56 +0530

    flutter learning part-2

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-        child: Text(
-        "Hello Flutter",
-        textDirection: TextDirection.ltr
-    ),//Text
-    )//Center
+    MaterialApp(
+      title: "My Flutter App",
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text("My First Flutter App"),
+            backgroundColor: Colors.amber),
+        body: Material(
+          color: Colors.amberAccent,
+          child: Center(
+            child: Text(
+              "Hello Flutter",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.black, fontSize: 40.0),
+            ), //Text
+          ), //Center
+        ), //Material
+      ), //Scaffold
+    ), //MaterialApp
   );
 }
